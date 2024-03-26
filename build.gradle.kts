393263102187f0dfb2fc3c85902ff2f8ba465024
qameta-ci <qameta-ci@qameta.io>
2024-03-26 08:07:12 +0000

    update 2024-03-26T08:07:12

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,14 @@ plugins {
 }
 
 tasks.withType(Wrapper::class) {
-    gradleVersion = "8.6"
+    gradleVersion = "8.7"
 }
 
 group = "com.example.junit5"
 version = "1.0-SNAPSHOT"
 
-val allureVersion = "2.25.0"
-val aspectJVersion = "1.9.21"
+val allureVersion = "2.26.0"
+val aspectJVersion = "1.9.22"
 val kotlinVersion = "1.9.23"
 
 kotlin {
