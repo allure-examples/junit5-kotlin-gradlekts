2c19f1f041ac0cb1c19f8761b57c8430dbdad1a0
qameta-ci <qameta-ci@qameta.io>
2024-11-06 16:52:26 +0000

    update 2024-11-06T16:52:26

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 plugins {
-    kotlin("jvm") version "1.9.23"
+    kotlin("jvm") version "1.9.25"
 }
 
 tasks.withType(Wrapper::class) {
-    gradleVersion = "8.7"
+    gradleVersion = "8.10.2"
 }
 
 group = "com.example.junit5"
 version = "1.0-SNAPSHOT"
 
-val allureVersion = "2.26.0"
+val allureVersion = "2.29.0"
 val aspectJVersion = "1.9.22"
-val kotlinVersion = "1.9.23"
+val kotlinVersion = "1.9.25"
 
 kotlin {
     jvmToolchain(17)
@@ -40,11 +40,11 @@ dependencies {
     testImplementation(platform("io.qameta.allure:allure-bom:$allureVersion"))
     testImplementation("io.qameta.allure:allure-junit5")
 
-    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation(platform("org.junit:junit-bom:5.11.3"))
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
-    testImplementation("org.slf4j:slf4j-simple:2.0.12")
+    testImplementation("org.slf4j:slf4j-simple:2.0.16")
 }
 
 repositories {
